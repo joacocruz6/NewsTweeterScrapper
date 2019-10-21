9fbdaa4a98822148d67fa04e9825b4dd2a4ebead
Joaquin Cruz D <joacocruz6@gmail.com>
2019-10-21 15:16:56 -0300

    refactor(webscrapper): Make a function to get the content and parse it

--- src/webscraper.py
+++ src/webscraper.py
@@ -2,12 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import json
-def main(args):
-    url = args[1]
+
+def getContent(url:str):
+    """
+    Gets the content for a given url in the web
+    
+    Arguments: 
+        url {str} -- [
+            The url of the webpage to scrap
+        ]
+    
+    Returns:
+        [type] -- [
+            The content as a BeautifulSoup object
+        ]
+    """
     response = requests.get(url,timeout=5)
     content = BeautifulSoup(response.content,"html.parser")
+    return content
+
+def main(args):
+    url = args[1]
+    content = getContent(url)
     print(content)
     return 0
+
 
 if __name__ == "__main__":
     main(sys.argv)
