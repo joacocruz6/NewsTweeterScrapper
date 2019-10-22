3bd6e299ffb6ea782a575945ffb40414384c2016
Joaquin Cruz <joacocruz6@gmail.com>
2019-10-22 18:31:42 -0300

    feat(wordcloud): Can generate massive news media twitter wordclouds

--- src/statistics/wordclouds.py
+++ src/statistics/wordclouds.py
@@ -5,13 +5,39 @@ import pandas as pd
 import sys
 import re
 STOPWORDS = get_stop_words("es")
+# Appending some useless and non informatic words to the stopwords
+STOPWORDS.append("twitter")
+STOPWORDS.append("http")
+STOPWORDS.append("https")
+STOPWORDS.append("bit")
+STOPWORDS.append("ly")
+STOPWORDS.append("com")
+STOPWORDS.append("cl")
+STOPWORDS.append("www")
+STOPWORDS.append("tinyurl")
+STOPWORDS.append("Señal")
+STOPWORDS.append("pic")
+STOPWORDS.append("AHORA")
+STOPWORDS.append("meganoticias")
+STOPWORDS.append("Sigue")
+STOPWORDS.append("t13")
+STOPWORDS.append("lacuarta")
+STOPWORDS.append("YouTube")
+STOPWORDS.append("mega")
+STOPWORDS.append("vivo")
+STOPWORDS.append("AHORA24H")
+STOPWORDS.append("cronica")
+STOPWORDS.append("noticia")
+STOPWORDS.append("ÚLTIMO")
+STOPWORDS.append("24Play")
 PATH = "../bdUploader/exports.csv"
 LINK_REGEX = "http[s]{0,1}://\w+\.[ly|com|cl]/\w*"
 def main(args: list)->int:
     df = pd.read_csv(PATH,encoding="utf-8")
+    
     text = df["tweet_text"]
-    one_text = text[0]
-    wordcloud = WordCloud(stopwords=STOPWORDS).generate(one_text)
+    image_text = " ".join(my_text for my_text in text)
+    wordcloud = WordCloud(stopwords=STOPWORDS).generate(image_text)
     plt.imshow(wordcloud,interpolation="bilinear")
     plt.axis("off")
     plt.savefig("wordcloud.png")
