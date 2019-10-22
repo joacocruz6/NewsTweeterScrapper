4d162869dd71abb52d51a0084880ea3a5a2e8c19
Joaquin Cruz <joacocruz6@gmail.com>
2019-10-21 22:28:46 -0300

    refactor(global): formatted the python files

--- src/bdUploader/uploader.py
+++ src/bdUploader/uploader.py
@@ -2,31 +2,36 @@ import json
 import psycopg2
 import sys
 from variables import USERNAME, PASSWORD
-HOST = "127.0.0.1" # localhost
+HOST = "127.0.0.1"  # localhost
 DBNAME = "postgres"
 PORT = "5432"
 DATA_PATH = '../jsonData/'
+
+
 def main(args: list) -> int:
     jfile = DATA_PATH + args[1]
     try:
-        connection = psycopg2.connect(user=USERNAME,password = PASSWORD, host = HOST, port = PORT, database = DBNAME)
+        connection = psycopg2.connect(
+            user=USERNAME, password=PASSWORD, host=HOST, port=PORT, database=DBNAME)
         cursor = connection.cursor()
-        with open(jfile,'r') as read_data:
+        with open(jfile, 'r') as read_data:
             raw_data = json.load(read_data)
             for data in raw_data:
                 query = """INSERT INTO scrapper.tweets ("tweet_id","user",hour,tweet_text) VALUES (%s,%s,%s,%s)"""
-                record_to_insert = (data['user'],data['date'],data['text'])
-                cursor.execute(query,record_to_insert)
+                record_to_insert = (data['user'], data['date'], data['text'])
+                cursor.execute(query, record_to_insert)
                 connection.commit()
                 count = cursor.rowcount
-                print(count,"Record inserted into the table")
+                print(count, "Record inserted into the table")
     except (Exception, psycopg2.Error) as error:
-        print("Error while connection to Postgres",error)
+        print("Error while connection to Postgres", error)
     finally:
         if connection:
             cursor.close()
             connection.close()
             print("Postgres connection closed")
-        return 0    
+        return 0
+
+
 if __name__ == "__main__":
     main(sys.argv)
--- src/webscrapper/webscraper.py
+++ src/webscrapper/webscraper.py
@@ -3,65 +3,78 @@ import requests
 import sys
 import json
 ROUTE = '../jsonData/'
-def getContent(url:str):
+
+
+def getContent(url: str):
     """
     Gets the content for a given url in the web
-    
+
     Arguments: 
         url {str} -- [
             The url of the webpage to scrap
         ]
-    
+
     Returns:
         [type] -- [
             The content as a BeautifulSoup object
         ]
     """
-    response = requests.get(url,timeout=5)
-    content = BeautifulSoup(response.content,"html.parser")
+    response = requests.get(url, timeout=5)
+    content = BeautifulSoup(response.content, "html.parser")
     return content
-def getData(content)->list:
+
+
+def getData(content) -> list:
     """Get the important data of the tweeter scrapper for the json document
-    
+
     Arguments:
         content {[type]} -- The content of the tweeter account
-    
+
     Returns:
         list -- list of dictionaries with three keys -name,date,text- on which the data will be dump on
     """
-    tweets = content.findAll('div',attrs = {"class" : "tweet"})
+    tweets = content.findAll('div', attrs={"class": "tweet"})
     data = list()
     for tweet in tweets:
         tweet_data = dict()
-        tweet_content = tweet.find('div',attrs = {"class" : "content"})
+        tweet_content = tweet.find('div', attrs={"class": "content"})
         print(tweet.attrs['data-tweet-id'])
         tweet_data["id"] = tweet.attrs['data-tweet-id']
-        user_name = tweet_content.find('a',attrs = {"class" : "account-group"}).find("strong",attrs={"class" : "fullname"}).text
+        user_name = tweet_content.find('a', attrs={
+                                       "class": "account-group"}).find("strong", attrs={"class": "fullname"}).text
         tweet_data["user"] = user_name
-        date_published = tweet_content.find('a',attrs={"class" : "tweet-timestamp"}).attrs['title']
+        date_published = tweet_content.find(
+            'a', attrs={"class": "tweet-timestamp"}).attrs['title']
         tweet_data["date"] = date_published
-        tweet_text = tweet_content.find('p', attrs={"class" : "tweet-text"}).text
+        tweet_text = tweet_content.find(
+            'p', attrs={"class": "tweet-text"}).text
         tweet_data['text'] = tweet_text
         data.append(tweet_data)
     return data
-def dump_json(data: list,jfile: str)->None:
+
+
+def dump_json(data: list, jfile: str) -> None:
     """Dump the data to a json file to be uploaded on the DB afterwards
-    
+
     Arguments:
         data {list} -- the data that will be dumped
         jfile {str} -- The route to the file that will be dumped the data on
     Returns:
         None -- Creates and dump the data on the json file
     """
-    with open(jfile,'w') as output:
-        json.dump(data,output)
+    with open(jfile, 'w') as output:
+        json.dump(data, output)
     return
-def main(args: list)->int:
+
+
+def main(args: list) -> int:
     url = args[1]
     content = getContent(url)
     data = getData(content)
     json_file = ROUTE + args[2]
-    dump_json(data,json_file)
+    dump_json(data, json_file)
     return 0
+
+
 if __name__ == "__main__":
-    main(sys.argv) # argv must contain in first position the webpage and in the second position the json file name with it's extension
+    main(sys.argv)  # argv must contain in first position the webpage and in the second position the json file name with it's extension
