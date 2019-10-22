0f3d45d57306d18b43f3aa711de6e2becb7693e2
Joaquin Cruz <joacocruz6@gmail.com>
2019-10-22 19:12:01 -0300

    feat(runner): Scraps within an hour and it's messages separate between each scrapping execution

--- runner.py
+++ runner.py
@@ -1,6 +1,8 @@
 import time
 import os
-HOUR = 60
+HOUR = 3600
 while True:
     os.system("bash SCRAP.sh")
+    print("--------------------------------------")
+    print("See you in an hour")
     time.sleep(1*HOUR)
