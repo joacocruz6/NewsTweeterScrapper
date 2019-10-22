dd54ebbe3098d2ee1d2e1444263f034fed33cec0
Joaquin Cruz <joacocruz6@gmail.com>
2019-10-22 18:04:43 -0300

    feat(wordcloud): Generate wordcloud from one phrase

--- src/statistics/wordclouds.py
+++ src/statistics/wordclouds.py
@@ -1,13 +1,20 @@
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud
+from stop_words import get_stop_words
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 import re
+STOPWORDS = get_stop_words("es")
 PATH = "../bdUploader/exports.csv"
 LINK_REGEX = "http[s]{0,1}://\w+\.[ly|com|cl]/\w*"
 def main(args: list)->int:
     df = pd.read_csv(PATH,encoding="utf-8")
-    print(df[2])
+    text = df["tweet_text"]
+    one_text = text[0]
+    wordcloud = WordCloud(stopwords=STOPWORDS).generate(one_text)
+    plt.imshow(wordcloud,interpolation="bilinear")
+    plt.axis("off")
+    plt.savefig("wordcloud.png")
     return 0
 
 
